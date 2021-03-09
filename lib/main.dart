0b49d895b36d46631b6ebc154bc4c50d67a22d70
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2021-03-09 22:58:22 +0800

    Added saved route

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Word = WordPair.random();
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.blueAccent),
-      home: Scaffold(
-        appBar: AppBar(title: Center(
-            child: Text("Word Generator"))),
-        body: Center(
-            child: Text(Word.asPascalCase)),
-      )
-    );
+      home: RandomWords()
+      );
   }
 }
+
+
---
+++ lib/random_words.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+    final randomWords = <WordPair>[];
+    final savedWords = Set<WordPair>();
+
+  Widget _buildList(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(10),
+      itemBuilder: (context, item){
+        if (item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= randomWords.length) {
+          randomWords.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(randomWords[index]);
+      },
+    );
+  }
+
+  Widget _buildRow (WordPair pair) {
+    final alreadySaved = savedWords.contains(pair);
+
+    return ListTile(
+      title: Text (
+        pair.asPascalCase,
+        style: TextStyle (fontSize: 18.0),
+      ),
+      trailing: Icon (
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            savedWords.remove(pair);
+          } else {
+            savedWords.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void _saved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context)
+    {
+      final Iterable <ListTile> tiles = savedWords.map((WordPair pair) {
+        return ListTile(
+            title: Text(pair.asPascalCase,
+                style: TextStyle(fontSize: 16.0)));
+      });
+      final List<Widget> divided =
+      ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+            title: Text("Saved Words")),
+        body: ListView(children: divided),
+      );
+    }));
+  }
+
+  Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+            title: Center(
+            child: Text("Word Generator")
+            ),
+            actions: [
+              IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _saved)
+          ],
+        ),
+        body: _buildList(),
+      );
+    }
+}
